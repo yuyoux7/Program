e7422521045fd43cc9da4a93bbfac3209bb0eb7e
宇佑 蕭 <yuyoux7@gmail.com>
2024-10-16 21:42:02 +0800

    UpDate IO Lib

--- Arduino_IO.cpp
+++ Arduino_IO.cpp
@@ -1,10 +1,79 @@
 #include "Arduino_IO.h"
+template<typename C, typename ...L>
+IO<C, L...>::IO()
+{
+};
 
-IO::IO(spio fn, spio PIN, ...)
+template<typename C, typename ...L>
+void IO<C, L...>::being(spio pin, type SP_TYPE, L ...Other)
+{
+	Pin[pin] = pin;
+	TYPE[pin] = SP_TYPE;
+	if constexpr (((sizeof...(L) % 2) == NULL) || (sizeof...(L) > 1))
+	{
+		being(Other...);
+	}
+};
+
+template<typename C, typename ...L>
+void IO<C, L...>::being(spio pin, L ...Other)
+{
+	Pin[pin] = pin;
+	TYPE[pin] = BOTH;
+	if constexpr (((sizeof...(L) % 2) == NULL) || (sizeof...(L) > 1))
+	{
+		being(Other...);
+	}
+};
+
+template<typename C, typename ...L>
+bool IO<C, L...>::write(spio pin, spio Power, L ...Other)
+{
+	if constexpr ((((sizeof...(L) % 2) == NULL) && signal(pin)))
+	{
+		//do something	
+	}
+	else if constexpr ((sizeof...(L) > 1 && signal(pin)))
+	{
+		//do something->same up
+		write(Other...);
+	}
+	else
+	{
+		return false;
+	}
+	return true;
+};
+
+template<typename C, typename ...L>
+C IO<C, L...>::read(spio pin, L ...Other)
+{
+	if constexpr (sizeof...(L) == NULL && signal(pin))
+	{
+		//do something	
+	}
+	else if (signal(pin))
+	{
+		//do something->same up
+		read(Other...);
+		return C();
+	}
+};
+
+template<typename C, typename ...L>
+IO<C, L...>::~IO()
 {
 
-}
-IO::~IO()
-{
+};
 
-}
+template<typename C, typename ...L>
+bool IO<C, L...>::signal(spio pin)
+{
+	C in_data{};
+	C range{};
+	if (Pin[pin] == pin)
+	{
+		return in_data >= range ? true : false;
+	}
+	return false;
+};
--- Arduino_IO.h
+++ Arduino_IO.h
@@ -1,17 +1,35 @@
 #pragma once
 #ifndef __ANDUINO_IO__
 #define __ARDUINO_IO__
-typedef uint8_t spio;
-#define IN (spio) 0
-#define OUT (spio) 1
-#define BOTH (spio) 2
+typedef uint16_t spio;
+typedef uint8_t type;
+#define IN (type) 0
+#define OUT (type) 1
+#define BOTH (type) 2
+template <typename C, typename ...L>
 class IO
 {
-public:
-	IO(spio fn = BOTH, spio PIN, ...);
-	~IO();
+	eunm{
+		A0 = 15,
+		A1 = 16,
+		A2 = 17,
+		A3 = 18,
+		A4 = 19,
+		A5 = 20
+	};
 
+public:
+	IO();
+	void being(spio pin, type SP_TYPE = BOTH, L ...Other);
+	void being(spio pin, L ...Other);
+	bool write(spio pin, spio Power, L ...Other);
+	C read(spio pin, L ...Other);
+	~IO();
+	
 private:
+	bool signal(spio pin);
+	spio Pin[30]{};
+	type TYPE[30]{};
 
 };
 #endif
