3f1976a1231e92dfabeffa53db919f953374f0ec
宇佑 蕭 <yuyoux7@gmail.com>
2024-11-04 21:46:53 +0800

    write main.ino

--- main.ino
+++ main.ino
@@ -1,5 +1,5 @@
-#include "Arduino_IO.h"
-
+#include "RGB.h"
+RGB RGB_Data(2, 3, 7, 8, 6);
 void setup()
 {
 
@@ -7,5 +7,7 @@ void setup()
 
 void loop()
 {
-
+	RGB_Data.Getlevel(RED);
+	RGB_Data.Getlevel(GREEN);
+	RGB_Data.Getlevel(BLUE);
 }
