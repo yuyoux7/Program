b4fe8eece5998fd4ac99a487f186be895901b53d
宇佑 蕭 <yuyoux7@gmail.com>
2024-11-20 20:29:01 +0800

    write motor lib

--- Arduino_motor.h
+++ Arduino_motor.h
@@ -1,26 +1,41 @@
 #pragma once
-#if define(Arduino)
-typedef uint16_t motor_pint;
-#else
-typedef int motor_pint;
-#endif 
+typedef uint8_t motor_pint;
 
 class motor
 {
 public:
-	motor(motor_pint right, motor_pint left);
+	motor(motor_pint ENA, motor_pint DIR, motor_pint PUI);
+	void setstep(motor_pint step);
+	void run();
+	void stop();
 	~motor();
 
 private:
-	motor_pint right_pin{};
-	motor_pint left_pin{};
-
+	motor_pint ENA_pin{};
+	motor_pint DIR_pin{};
+	motor_pint PUI_pin{};
+	motor_pint Step{};
 };
 
-motor::motor(motor_pint right, motor_pint left) : right_pin(right), left_pin(left)
+motor::motor(motor_pint ENA, motor_pint DIR, motor_pint PUI) : ENA_pin(ENA), DIR_pin(DIR), PUI_pin(PUI)
 {
 
-};
+}
+
+void motor::setstep(motor_pint step)
+{
+
+}
+
+void motor::run()
+{
+
+}
+
+void motor::stop()
+{
+
+}
 
 motor::~motor()
 {
